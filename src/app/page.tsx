a621e9e2e4a5dba8ada9f0792c166fdfa0c761e4
kunal89204 <kunalkhandelwal108@gmail.com>
2025-01-09 21:18:53 +0530

    first push

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Box , Button} from '@chakra-ui/react'
+import React from 'react'
+import Hero from '@/app/components/Hero'
+
+const Home = () => {
+  return (
+    <Box>
+      <Hero />
+      hello 
+      <Button>Hi</Button>
+    </Box>
+  )
+}
+
+export default Home
