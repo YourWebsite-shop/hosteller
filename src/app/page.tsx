af11b3464c8a5d064bbd4202023f650bcc4d3b76
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2025-01-10 00:15:29 +0530

    enjoystay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Box , Button} from '@chakra-ui/react'
 import React from 'react'
 import Hero from '@/components/Hero'
+import EnjoyStay from '@/components/EnjoyStay'
 
 const Home = () => {
   return (
     <Box>
       <Hero />
+      <EnjoyStay />
     </Box>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,32 +1,113 @@
 import React from "react";
-import { Box, Text, Button } from "@chakra-ui/react";
+import { Box, Text, Button, Flex } from "@chakra-ui/react";
+import { base, col } from "framer-motion/client";
 
 const Hero: React.FC = () => {
   return (
-    <Box bg={"#181818"}>
-      <Box maxWidth={"1216px"}>
+    <Box bg="#181818">
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        minHeight="100vh"
+        textAlign="center"
+        pt={{base:"180px", sm:"220px"}}
+      >
         <Text
           fontSize={{ base: "36px", sm: "48px", md: "60px" }}
           fontWeight="bold"
           color="white"
-          mb="6"
-          className="animate__animated animate__fadeInDown"
+          mb="4"
+          lineHeight={{ base: "1.1", sm: "1.2", md: "1.3" }}
         >
           Welcome to Weekends Forever
-          <Text as="span" display="block" color="yellow.400" mt="2">
+          <Text textAlign="center" as="span" display="block" color="yellow.400">
             Rishikesh
           </Text>
         </Text>
 
-        <Text>
+        <Text
+          fontSize={{ base: "lg", sm: "xl" }}
+          color="gray.300"
+          maxWidth="3xl"
+          mx="auto"
+          mb="8"
+        >
           Your perfect escape in the serene landscapes of Rishikesh. Located in
           the heart of Tapovan, just 6 minutes from Laxman Jhula.
         </Text>
-        <Box>
-          <Button>Book Now</Button>
-          <Button>Learn More</Button>
+
+        <Box
+          display="flex"
+          flexDirection={{ base: "column", sm: "row" }}
+          gap="16px"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button
+            px="8"
+            py="6"
+            bg="#ECC94B"
+            color="#1A202C"
+            borderRadius="full"
+            fontWeight="semibold"
+            _hover={{ bg: "yellow.400" }}
+          >
+            Book Now
+          </Button>
+
+          <Button
+            px="8"
+            py="6"
+            border="2px"
+            bg="#181818"
+            borderColor="white"
+            color="white"
+            borderRadius="full"
+            _hover={{
+              bg: "white",
+              color: "#1A202C",
+            }}
+          >
+            Learn More
+          </Button>
         </Box>
-      </Box>
+
+        {/* Bottom Flex (pushed to the bottom) */}
+        <Flex
+          flexDirection="row"
+          gap="32px"
+          marginTop="auto" // Pushes this section to the bottom of the page
+          mb="8px"
+        >
+          <Flex direction="column" align="center">
+            <Text color="#e5e7eb" fontSize="4xl" fontWeight="bold">
+              4.1
+            </Text>
+            <Text color="#e5e7eb" fontSize="sm">
+              Guest Rating
+            </Text>
+          </Flex>
+
+          <Flex direction="column" align="center">
+            <Text color="#e5e7eb" fontSize="4xl" fontWeight="bold">
+              15+
+            </Text>
+            <Text color="#e5e7eb" fontSize="sm">
+              Rooms
+            </Text>
+          </Flex>
+
+          <Flex direction="column" align="center">
+            <Text color="#e5e7eb" fontSize="4xl" fontWeight="bold">
+              24/7
+            </Text>
+            <Text color="#e5e7eb" fontSize="sm">
+              Support
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
     </Box>
   );
 };
